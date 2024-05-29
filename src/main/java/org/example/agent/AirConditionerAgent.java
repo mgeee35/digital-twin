1fde6b9c1273bd7c96e67d82a48b044af49b6280
Mustafa Tufan <mustafa@tufan.ee>
2024-05-30 00:32:59 +0300

    changes

--- src/main/java/org/example/agent/AirConditionerAgent.java
+++ src/main/java/org/example/agent/AirConditionerAgent.java
@@ -1,29 +1,12 @@
 package org.example.agent;
 
 import jade.core.Agent;
-import jade.core.behaviours.TickerBehaviour;
-import jade.lang.acl.ACLMessage;
-import java.util.Random;
+import org.example.behaviour.AirConditionerTickerBehaviour;
 
 public class AirConditionerAgent extends Agent {
-    private Random random = new Random();
 
     protected void setup() {
         System.out.println("AirConditionerAgent " + getAID().getLocalName() + " is ready.");
-
-        addBehaviour(new TickerBehaviour(this, 5000) { // every 5 seconds
-            protected void onTick() {
-                int sensorValue = random.nextInt(100); // Random sensor value between 0 and 99
-                System.out.println("Sensor value: " + sensorValue);
-
-                if (sensorValue < 20 || sensorValue > 80) { // Assuming abnormal values are below 20 or above 80
-                    System.out.println("Abnormal sensor value detected: " + sensorValue);
-                    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-                    msg.addReceiver(getAID("DFAgent"));
-                    msg.setContent("AirConditionerFailure");
-                    send(msg);
-                }
-            }
-        });
+        addBehaviour(new AirConditionerTickerBehaviour(this, 1000));
     }
 }
--- src/main/java/org/example/agent/DFAgent.java
+++ src/main/java/org/example/agent/DFAgent.java
@@ -1,30 +1,11 @@
 package org.example.agent;
 
 import jade.core.Agent;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
+import org.example.behaviour.DFAgentCyclicBehaviour;
 
 public class DFAgent extends Agent {
     protected void setup() {
         System.out.println("DFAgent " + getAID().getLocalName() + " is ready.");
-
-        addBehaviour(new jade.core.behaviours.CyclicBehaviour() {
-            public void action() {
-                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-                ACLMessage msg = receive(mt);
-                if (msg != null) {
-                    String content = msg.getContent();
-                    if (content.equals("AirConditionerFailure")) {
-                        System.out.println("Received air conditioner failure alert.");
-                        ACLMessage stopMsg = new ACLMessage(ACLMessage.REQUEST);
-                        stopMsg.addReceiver(getAID("MachineAgent"));
-                        stopMsg.setContent("StopMachine");
-                        send(stopMsg);
-                    }
-                } else {
-                    block();
-                }
-            }
-        });
+        addBehaviour(new DFAgentCyclicBehaviour(this));
     }
 }
